aa6cba1871d309efbd241a6dc684b613364e4fbb
sainallamilli17 <sainallamilli17@gmail.com>
2025-03-09 11:53:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,72 +2,40 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
+BOT_TOKEN = "7543846429:AAF5GDFBCDsM_tghg6g3MXnZW_Ol5Xx5Occ"
+API_ID = 979826
+API_HASH = "238183386c30590d073b457166ba260d"
 
+OWNER_ID = 1074804932
+DB_URL = "mongodb+srv://ygovcu:fY1f9Wovol3NqhUX@cluster0.1mdno.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DB_NAME = "madflixbotz"
 
+CHANNEL_ID = -1001847420676
+FORCE_SUB_CHANNEL = -1002358532189
+FORCE_SUB_CHANNEL2 = 0
+FORCE_SUB_CHANNEL3 = 0
+FORCE_SUB_CHANNEL4 = 0
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+FILE_AUTO_DELETE = 600  # auto delete in seconds
 
-
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
-
-
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))
-FORCE_SUB_CHANNEL3 = int(os.environ.get("FORCE_SUB_CHANNEL3", "0"))
-FORCE_SUB_CHANNEL4 = int(os.environ.get("FORCE_SUB_CHANNEL4", "0"))
-
-
-FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "600")) # auto delete in seconds
-
-
-PORT = os.environ.get("PORT", "8080")
-TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
-
-
+PORT = "8080"
+TG_BOT_WORKERS = 4
 
 try:
-    ADMINS=[6848088376]
+    ADMINS = [6848088376]
     for x in (os.environ.get("ADMINS", "6848088376").split()):
         ADMINS.append(int(x))
 except ValueError:
-        raise Exception("Your Admins list does not contain valid integers.")
+    raise Exception("Your Admins list does not contain valid integers.")
 
-
-
-
-
-
-
-
-
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
-
-PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
-
-DISABLE_CHANNEL_BUTTON = True if os.environ.get('DISABLE_CHANNEL_BUTTON', "True") == "True" else False
+CUSTOM_CAPTION = None
+PROTECT_CONTENT = False
+DISABLE_CHANNEL_BUTTON = True
 
 BOT_STATS_TEXT = "<b>BOT UPTIME :</b>\n{uptime}"
-
-
-
-
-
-
-
 USER_REPLY_TEXT = "❌Don't Send Me Messages Directly I'm Only File Share Bot !"
-
-START_MSG = os.environ.get("START_MESSAGE", "Hello {mention}\n\nI Can Store Private Files In Specified Channel And Other Users Can Access It From Special Link.")
-
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {mention}\n\n<b>You Need To Join In My Channel/Group To Use Me\n\nKindly Please Join Channel</b>")
-
-
-
-
+START_MSG = "Hello {mention}\n\nI Can Store Private Files In Specified Channel And Other Users Can Access It From Special Link."
+FORCE_MSG = "Hello {mention}\n\n<b>You Need To Join In My Channel/Group To Use Me\n\nKindly Please Join Channel</b>"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(6848088376)
@@ -89,14 +57,8 @@ logging.basicConfig(
 )
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
-   
-
-
-
-
 
 # Jishu Developer 
 # Don't Remove Credit 🥺
